651e042ed3b4f9f7dcb37b6da08ac36525b80b67
Abdul Basit <abdul.basit@koderlabs.com>
2023-07-22 14:47:36 +0500

    Remove index from key and use id

--- src/components/GistList/GistList.js
+++ src/components/GistList/GistList.js
@@ -50,8 +50,8 @@ export const GistList = () => {
 
   return (
     <GistListWrapper>
-      {gistsList.map((gist, index) => (
-        <Gist {...gist} key={index} />
+      {gistsList.map((gist) => (
+        <Gist {...gist} key={gist.id} />
       ))}
     </GistListWrapper>
   );
